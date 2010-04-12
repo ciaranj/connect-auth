c73bd6ea9ca637923379ef9102964d61c97b3dde
ciaranj <ciaranj@gmail.com>
2010-04-12 19:11:56 +0100

    Made it all 'async-y' to fit with the rest of node.js :) Next up tests, followed by a re-factor.

--- examples/app.js
+++ examples/app.js
@@ -5,18 +5,26 @@ kiwi.require('express')
 require('express/plugins')
 global.merge(require('../lib/express/plugins/auth'));
 
-use(Cookie) 
+var authorize= function(user, pass, callback) {
+  var result= ( user == 'foo' && pass == 'bar' )
+  callback(null, result);
+}
 
+use(Cookie) 
 //use(Session, { dataStore: MongoDbStore, mongoDbName:'sessions_poop', lifetime: (15).seconds, reapInterval: (10).seconds })
 //use(Session, { dataStore: MongoDbStore, mongoServerPort: 27017, mongoServerAddress: "127.0.0.1", mongoDbName:'sessions_poop', lifetime: (15).seconds, reapInterval: (10).seconds })
 //use(Session, { dataStore: MongoDbStore, mongoServer:  new require('mongodb/connection').Server("127.0.0.1", 27017, {auto_reconnect: true}, {}) , lifetime: (15).seconds, reapInterval: (10).seconds })
 use(Session, { lifetime: (150).seconds, reapInterval: (10).seconds })
-use(Auth)
+use(Auth, {onAuthorize: authorize} )
 
 get('/', function() {
-  if( this.isAuthorized() ) {
-    if( ! this.session.counter ) this.session.counter= 0;
-    return "<h1>Hello!"+ this.REMOTE_USER+ "</h1>"  + "<p>" + (this.session.counter++) +"</p>"
-  }
+  var self=this;
+  self.isAuthorized( function(error, authorized) { 
+    if( authorized ) {
+      if( ! self.session.counter ) self.session.counter= 0;
+      self.status(200)  
+      self.respond("<h1>Hello!"+ self.REMOTE_USER+ "</h1>"  + "<p>" + (self.session.counter++) +"</p>")
+    }
+  });
 })
 run();
