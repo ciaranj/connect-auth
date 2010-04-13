76217b541beeae4179b9ae8042afd9fc90787476
ciaranj <ciaranj@gmail.com>
2010-04-13 23:17:05 +0100

    Ok, moved to the beginnings of a more strategy orientated architecture Currently tests are broken, but example works, offering both Digest + Basic HTTP auth.

--- examples/app.js
+++ examples/app.js
@@ -17,15 +17,20 @@ use(Session)
 //use(Session, { dataStore: MongoDbStore, mongoServerPort: 27017, mongoServerAddress: "127.0.0.1", mongoDbName:'sessions_poop', lifetime: (15).seconds, reapInterval: (10).seconds })
 //use(Session, { dataStore: MongoDbStore, mongoServer:  new require('mongodb/connection').Server("127.0.0.1", 27017, {auto_reconnect: true}, {}) , lifetime: (15).seconds, reapInterval: (10).seconds })
 use(Session, { lifetime: (150).seconds, reapInterval: (10).seconds })
-use(DigestAuth, {getPasswordForUser: getPasswordForUserFunction} )
+use(Auth, {getPasswordForUser: getPasswordForUserFunction} )
+
 
 get('/', function() {
   var self=this;
-  self.isAuthorized( function(error, authorized) { 
-    if( authorized ) {
+  self.authenticate(function(error, authenticated) { 
+    if( authenticated ) {
       if( ! self.session.counter ) self.session.counter= 0;
       self.status(200)  
       self.respond("<h1>Hello!"+ self.session.auth.REMOTE_USER+ "</h1>"  + "<p>" + (self.session.counter++) +"</p>")
+    }
+    else {
+      self.status(200)  
+      self.respond("<h1>Who are you, you seem to be un-authenticateable</h1>")
     }
   });
 })
