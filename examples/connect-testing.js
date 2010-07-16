1fe69c4463657be9176ba9c5e4998100f185de11
ciaranj <ciaranj@gmail.com>
2010-07-17 00:18:46 +0100

    Delegating Http (Digest & Basic) authentication now works again.

--- examples/connect-testing.js
+++ examples/connect-testing.js
@@ -6,6 +6,7 @@ var Anonymous= require('../lib/auth.strategies/anonymous');
 var Never= require('../lib/auth.strategies/never');
 var Basic= require('../lib/auth.strategies/http/basic')
 var Digest= require('../lib/auth.strategies/http/digest')
+var Http= require('../lib/auth.strategies/http/http')
 
 var sys= require('sys')  
 
@@ -19,7 +20,7 @@ var getPasswordForUserFunction= function(user,  callback) {
 
 
 function helloWorld(req, res) {
-   req.authenticate(['digest'], function(error, authenticated) { 
+   req.authenticate(['http'], function(error, authenticated) { 
      if( authenticated ) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('hello world - AUTHENTICATED');
@@ -35,6 +36,7 @@ connect.createServer( connect.cookieDecoder(),
                       connect.session({ store: new MemoryStore({ reapInterval: -1 }) }),
                       auth({"basic": new StrategyDefinition(Basic,{getPasswordForUser: getPasswordForUserFunction}),
                             "digest": new StrategyDefinition(Digest,{getPasswordForUser: getPasswordForUserFunction}),
+                            "http": new StrategyDefinition(Http, {getPasswordForUser: getPasswordForUserFunction}),
                             "anon": new StrategyDefinition(Anonymous),
                             "never": new StrategyDefinition(Never)}), 
                       helloWorld)
