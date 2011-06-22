ciaranj/connect-auth

commit a7811e259152125451afd87eecb24a4d5f90bbbb

Add errorResponse to getAuthDetails

lib/index.js
@@ -88,7 +88,8 @@ Auth= module.exports = function(optionsOrStrategy) {
           //TODO: DEFINE DEFAULT STRATEGY(IES)
         }
         if( isAuthenticated(req, scope) ) callback(null, true);
-        else strategyExecutor.authenticate(strategy, scope, req, res, function(error, executionResult){
+        else strategyExecutor.authenticate(strategy, scope, req, res, function (error, executionResult) {
+          req.getAuthDetails().errorResponse= executionResult.errorResponse;
           if(error) callback(error); // I really wish the plugins code logged errors.. 
           else {
             if( !executionResult.user ) callback(null,false)

commit ee85efbf59d726641c96c767d8cc20eb2e0763d3

Fixed bug introduced in a7811e259152125451afd87eecb24a4d5f90bbbb
Not sure quite what the purpose of this errorResponse property is, but it was being set on an undefind variable

lib/index.js
@@ -89,7 +89,7 @@ Auth= module.exports = function(optionsOrStrategy) {
         }
         if( isAuthenticated(req, scope) ) callback(null, true);
         else strategyExecutor.authenticate(strategy, scope, req, res, function (error, executionResult) {
-          req.getAuthDetails().errorResponse= executionResult.errorResponse;
+          if( executionResult) { req.getAuthDetails().errorResponse= executionResult.errorResponse; }
           if(error) callback(error); // I really wish the plugins code logged errors.. 
           else {
             if( !executionResult.user ) callback(null,false)